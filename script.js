f7f66dd4c148d4828dfeb2cf31733c3e869eddc7
Nimira43 <n.rabone72@aol.com>
2025-08-02 13:47:20 +0100

    Asteroid class

--- script.js
+++ script.js
@@ -105,6 +105,18 @@ class Projectile {
   }
 }
 
+class Asteroid extends Enemy {
+  constructor(game) {
+    super(game)
+    this.image = document.getElementById('asteroid')
+    this.frameX = 0
+    this.frameY = Math.floor(Math.random() * 4)
+    this.maxFrames = 7
+    this.lives = 5
+    this.maxLives = this.lives
+  }
+}
+
 class Enemy {
   constructor(game) {
     this.game = game
